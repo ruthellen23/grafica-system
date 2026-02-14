de0927a7be7bf5794726ab89366f179a697cb854
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-02-14 19:50:05 -0300

    test: adicionando application-test.properties para suporte ao H2 no CI/CD

--- src/test/java/com/grafica/system/SecurityFullTest.java
+++ src/test/java/com/grafica/system/SecurityFullTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,8 +13,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class SecurityFullTest {
-    @Autowired private MockMvc mockMvc;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @ParameterizedTest
     @ValueSource(strings = {"/usuarios", "/relatorios/vendas-mensais", "/clientes", "/pedidos"})
